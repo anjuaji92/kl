35db0d3a0fbee355d689bbd5193f0e59f13e1d4b
anjuaji92 <97083740+anjuaji92@users.noreply.github.com>
2022-01-22 11:44:42 +0530

    new changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 let email=document.getElementById("email");
 let pwd=document.getElementById("pwd");
 let pwd1=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-
+let email1=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3}).([a-z]{2,3}?)$/
 function validate()
 {
     // if(email.value==""||pwd.value=="")
@@ -15,14 +15,19 @@ function validate()
     //     pwd.style.border="2px solid red";
     //     return false;
     // }
-    if(pwd.value.match(pwd1))
+    if(email.value.match(email1))
+    {
+         return true;
+    }
+    else if(pwd.value.match(pwd1))
     {
         alert("success")
         pwd.style.border="1px solid grey";
         return  true;
     }
+    
     else
     alert("password should contain atleast 1 Uppercase,lowercase,digit and special character");
-    pwd.style.border="2px solid red";
+    // pwd.style.border="2px solid red";
     return false;
 }
